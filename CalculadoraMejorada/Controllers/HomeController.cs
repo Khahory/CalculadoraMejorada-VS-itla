7b6d215a73ba86d5394437345b31a1a181249614
KhahoryITLA <20164247@itla.edu.do>
2019-06-08 19:52:58 -0400

    Commit

---
+++ CalculadoraMejorada/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using CalculadoraMejorada.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraMejorada.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Calcular()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calcular(CalculadoraModel model, string valor)
+        {
+            switch (valor)
+            {
+                case "+": model.resultado = model.ola + model.Numero2;
+                    break;
+            }
+
+            return View(model);
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CalculadoraMejorada/Models/CalculadoraModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraMejorada.Models
+{
+    public class CalculadoraModel
+    {
+
+        // el nombre de la etiiqueta html sabe que le pertenece a este modelo, buscara el nombre a esta variable
+        public int ola { get; set; }
+        public int Numero2 { get; set; }
+        public int resultado{ get; set; }
+    }
+}
